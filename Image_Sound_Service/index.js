035262797ea5e09177e721952d8be76614086faa
Rellow09-code <2769364@students.wits.ac.za>
2025-09-04 01:12:55 +0200

    vercel deployment: Attempt4 alter vercel.json structure1

--- Image_Sound_Service/index.js
+++ Image_Sound_Service/index.js
@@ -163,6 +163,4 @@ app.post('/speak', async (req, res) => {
 app.get('/', async (req, res) => {
     res.status(200).json({response:`The server is running perfectly, ready to serve. whether your request is about images or speech`});
 });
-
-//export app as a serverless app for vercel.
 module.exports = app;
